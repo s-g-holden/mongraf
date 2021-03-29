ae8807bb2c789d364f0279b0168b94d531680284
s-g-holden <sholden@worth.systems>
2021-03-29 09:01:49 +0100

    bamboo & python requirements

--- expyter/src/exporter.py
+++ expyter/src/exporter.py
@@ -1,32 +1,24 @@
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Summary, Info
 import random
 import time
+import os
 from atlassian import Bamboo
 
 BAMBOO_URL = os.environ.get("BAMBOO_URL", "http://localhost:8085")
-ATLASSIAN_USER = os.environ.get("ATLASSIAN_USER", "admin")
-ATLASSIAN_PASSWORD = os.environ.get("ATLASSIAN_PASSWORD", "admin")
+ATLASSIAN_USER = os.environ.get("ATLASSIAN_USER", "bamboo")
+ATLASSIAN_PASSWORD = os.environ.get("ATLASSIAN_PASSWORD", "password")
 
 bamboo = Bamboo(url=BAMBOO_URL, username=ATLASSIAN_USER, password=ATLASSIAN_PASSWORD)
 
-agent_status = bamboo.agent_status()
-print(agent_status)
+PLAN_INFO = Info('bamboo_plan_info', 'General plan information.')
 
-activity = bamboo.activity()
-print(activity)
-
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
-# Decorate function with metric.
-@REQUEST_TIME.time()
-def process_request(t):
-    """A dummy function that takes some time."""
-    time.sleep(t)
+def plan_info_request(key):
+    """Request plan info."""
+    PLAN_INFO.info(bamboo.get_plan(key))
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
-        process_request(random.random())
+        plan_info_request('EX-EX')
